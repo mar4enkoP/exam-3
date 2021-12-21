943e732278e93d2bfbdad05b8f34da2ddc8a57f2
User Undefined <marchenkopavel2006@gmail.com>
2021-12-21 10:22:28 +0200

    Add project files.

---
+++ exam-3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "input number: ";
+int number, polin;
+cin >> number;
+int last,first;
+last = number % 10;
+first = (number / 100) % 10;
+if (first == last)
+cout << number << " This is palindrom" << endl;
+else
+cout << "NO " << endl;
+return 0;
+}
